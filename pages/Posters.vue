32be1667172cac0694957878ac85e96653136dd0
Yegor Knyazev <egorknyazev10@gmail.com>
2023-12-01 18:13:21 +0400

    tryings

--- pages/Posters.vue
+++ pages/Posters.vue
@@ -1,12 +1,11 @@
 <script setup>
-
 import { useRoute } from "vue-router";
 import { usePoster } from '~/store/poster';
 import { useLocations } from '~/store/locations';
 
 let posterStore = usePoster()
 
-const wrapper = ref(null)
+// const wrapper = ref(null)
 const route = useRoute();
 let locationsStore = useLocations()
 let imgOnload = ref(false)
@@ -22,67 +21,61 @@ let handleScroll = async () => {
     triggerHeight = wrapper.value.scrollHeight
   }
   if (triggerHeight == wrapper.value.scrollHeight) {
-    await posterStore.fetchPosters(posterStore.filter)
+    // await posterStore.fetchPosters(posterStore.filter)
   }
 }
 
 onMounted(async () => {
-  if (route.hash) {
-    let id = route.hash.slice(1)
-    document.getElementById(id)?.scrollIntoView()
-  }
-  wrapper.value.addEventListener("scroll", handleScroll);
-
-  if (posterStore.posters.length == 0) {
-    let filter
-    if (localStorage.getItem('filterForm')) {
-      filter = JSON.parse(localStorage.getItem('filterForm'))
+  console.log(process.client)
+    if (route.hash) {
+      let id = route.hash.slice(1)
+      document.getElementById(id)?.scrollIntoView()
     }
-    if (!locationsStore.location.length) {
-      if (localStorage.getItem('location')) {
-        locationsStore.location = localStorage.getItem('location')
-      }
-    }
-
-    posterStore.page = 1
-    await posterStore.fetchPosters(filter)
-  }
-
-
-});
-onBeforeMount(() => {
-  window.addEventListener("load", isLoad());
-}
-)
-onUpdated(() => {
-  window.addEventListener("load", isLoad());
+    // wrapper.value.addEventListener("scroll", handleScroll);
+  
+    // if (posterStore.posters.length == 0) {
+    //   let filter
+    //   if (localStorage.getItem('filterForm')) {
+    //     filter = JSON.parse(localStorage.getItem('filterForm'))
+    //   }
+    //   if (!locationsStore.location.length) {
+    //     if (localStorage.getItem('location')) {
+    //       locationsStore.location = localStorage.getItem('location')
+    //     }
+    //   }
+  
+    //   posterStore.page = 1
+    //   // await posterStore.fetchPosters(filter)
+    // }
 })
 </script>
 
 <template>
 
-  
-  <div class="wrapper" ref="wrapper" style="overflow-x: hidden;">
-    <v-container class="pt-0 d-flex justify-center ">
-      <v-row v-show="imgOnload" class="justify-center flex-wrap mb-16 mt-2 w-100">
-    
-        <v-col v-for="item of posterStore.posters" :key="item._id" cols="6" sm="4" md="3" lg="2" class="pa-1">
-          <PosterCard :poster="item" :id='item._id'/>
-        </v-col>
+  <!-- <ClientOnly>
+    <div class="wrapper" ref="wrapper" style="overflow-x: hidden;">
+      <v-container class="pt-0 d-flex justify-center ">
+        <v-row v-show="imgOnload" class="justify-center flex-wrap mb-16 mt-2 w-100">
       
+          <v-col v-for="item of posterStore.posters" :key="item._id" cols="6" sm="4" md="3" lg="2" class="pa-1">
+            <PosterCard :poster="item" :id='item._id'/>
+          </v-col>
+        
+        </v-row>
+      </v-container>
+      <v-row class="justify-center">
+        <v-col>
+          <h3 class="text-center" v-if="!posterStore.posters.length && posterStore.isLoaded"> Мы ничего не нашли 😟</h3>
+        </v-col>
       </v-row>
-    </v-container>
-    <v-row class="justify-center">
-      <v-col>
-        <h3 class="text-center" v-if="!posterStore.posters.length && posterStore.isLoaded"> Мы ничего не нашли 😟</h3>
-      </v-col>
-    </v-row>
-    <v-row class="justify-center">
-      <v-col v-show="!posterStore.isLoaded" cols="12" sm="4" class="ma-0 pa-0"> <v-progress-linear indeterminate
-          color="accent"></v-progress-linear></v-col>
-
-    </v-row>
-  </div>
+      <v-row class="justify-center">
+        <v-col v-show="!posterStore.isLoaded" cols="12" sm="4" class="ma-0 pa-0"> <v-progress-linear indeterminate
+            color="accent"></v-progress-linear></v-col>
+  
+      </v-row>
+    </div>
+  </ClientOnly> -->
+  афиши
 </template>
 
 <style lang="scss" scoped>
@@ -96,4 +89,4 @@ onUpdated(() => {
   }
 }
 
-</style>
+</style>~/stores/poster~/stores/locations
