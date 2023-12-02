676138bdad2382abe7adcc17cd833e9e0cc8a319
qbclub <17926057+qbclub@users.noreply.github.com>
2023-12-02 20:08:44 +0400

    storage exercise

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,27 @@
+<script setup>
+import { getData, setData } from 'nuxt-storage/local-storage';
+
+let data = ref(1)
+let data2 = ref()
+let click = () => {
+    data2.value++
+}
+
+watch(data2, () => {
+    data.value = data2.value
+    setData('data', data.value)
+})
+
+onMounted(() => {
+    data2.value = getData('data', data.value)
+    setData('data', data.value)
+})
+
+
+</script>
 <template>
     <div class="text-center">
-        start
+        <span @click="click()">start</span> 
+        {{ data}}
     </div>
-
 </template>
