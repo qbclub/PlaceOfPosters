87a8a96de6030ae6847a625042100874803456ab
qbclub <17926057+qbclub@users.noreply.github.com>
2023-12-30 13:38:01 +0400

    main meta

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
-  <NuxtPwaManifest />  
+  <Head>
+    <Title>Place of posters</Title>
+    <meta name="description"
+      content="PlPo:place of posters - место для ваших афиш. Спектакль, концерт, встреча, лекция, распродажа, экскурсия, кино - любое событие может стать плакатом на PlPo">
+    <meta name="keywords"
+      content="Спектакль, концерт, встреча, лекция, распродажа, экскурсия, кино, отдых, развлечение, плакат, афиша, мероприятия">
+    <meta property="og:title" content="PlPo:place of posters">
+    <meta property="og:description"
+      content="PlPo:place of posters - место для ваших афиш. Спектакль, концерт, встреча, лекция, распродажа, экскурсия, кино - любое событие может стать плакатом на PlPo">
+    <meta property="og:image" content="https://plpo.ru/android-chrome-512x512.png">
+    <meta property="og:url" content="https://plpo.ru/">
+    <meta property="og:type" content="website">
+  </Head>
+  <NuxtPwaManifest />
   <NuxtLayout>
   </NuxtLayout>
 </template>
