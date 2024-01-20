d4c624dc308c1e7ca91748688df03918df79cc9c
qbclub <17926057+qbclub@users.noreply.github.com>
2024-01-20 23:28:06 +0400

    remove scale

--- components/Miniature.vue
+++ components/Miniature.vue
@@ -13,9 +13,9 @@ let checkRout = () => {
 
 <template>
   <div class="card rounded" @click="navigateTo(`${checkRout()}?_id=${poster._id}`)">
-    <div class="img-container h-100">
-      <img :src="poster.image" alt="" class="h-100 rounded" :class="{ hide: poster.isHidden }">
-    </div>
+   
+      <v-img :src="poster.image" cover class="h-100 rounded" ></v-img>
+
   </div>
 </template>
 <style scoped lang="scss">
@@ -28,30 +28,10 @@ let checkRout = () => {
   position: relative;
   overflow: hidden;
 
-  .img-container {
-    position: absolute;
-    top: 0;
-    left: 0;
-
-    img {
-      -webkit-transition: 0.5s ease;
-      transition: 0.5s ease;
-
-    }
-  }
-
-  .hide {
-    filter: grayscale(100%);
-  }
 
 }
 
-@media (min-width: 900px) {
-  .card:hover .img-container img {
-    -webkit-transform: scale(1.1);
-    transform: scale(1.1);
-  }
-}
+
 
 
 .card-front {
