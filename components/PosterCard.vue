ee3b8d4131951cd89c8ffe6c6cc343d1028816a8
qbclub <17926057+qbclub@users.noreply.github.com>
2024-09-01 23:32:59 +0400

    pushkin`s card

--- components/PosterCard.vue
+++ components/PosterCard.vue
@@ -9,10 +9,17 @@ let checkRout = () => {
     return '/post'
   }
 }
+
+let isPuskinCard = computed(() => {
+  return poster.eventType.some((type) => type == 'пушкинская карта')
+})
+
 </script>
 
 <template>
+
   <div class="card rounded " @click="navigateTo(`${checkRout()}?_id=${poster._id}`)">
+    <img class="pcard" v-if='isPuskinCard' src="../assets/images/pcard.png">
     <v-img class="h-100" :src="poster.image" cover :class="{ hide: poster.isHidden }">
       <!-- <img :srcset="poster.image"  loading="lazy" alt="" class="h-100 rounded" :class="{ hide: poster.isHidden }"/> -->
 
@@ -32,7 +39,15 @@ let checkRout = () => {
   overflow: hidden;
   -webkit-transition: 0.5s ease;
   transition: 0.5s ease;
+}
 
+.pcard {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  border-radius: 50%;
+  z-index: 1;
+  width: 15%;
 }
 
 .hide {
