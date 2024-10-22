e223bfa6e311bc7bb34013dbf1d4788cc779dac4
regtyit <titov54275@gmail.com>
2024-10-22 18:12:39 +0400

    fix

--- middleware/is-manager.js
+++ middleware/is-manager.js
@@ -2,7 +2,6 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     if (process.server) return //пропускаем, когда код выполняется на сервере
 
     let authStore = useAuth()
-    console.log(authStore.user)
     if (!authStore.user?.managerIn?.length) {
         await authStore.checkAuth()
     }
