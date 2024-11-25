acd5d2141638be861889c5de6630ec9e7858a200
regtyit <titov54275@gmail.com>
2024-11-25 23:45:13 +0400

    chut SEO

--- app.vue
+++ app.vue
@@ -1,22 +1,32 @@
 <script setup>
-useHead({
+// useHead({
+//   title: 'Место для афиш',
+//   meta: [
+//     { name: 'description', content: 'PlPo:place of posters - место для ваших афиш. Спектакль, концерт, встреча, лекция, распродажа, экскурсия, кино - любое событие может стать плакатом на PlPo"' },
+//     { name: 'keywords', content: 'Спектакль, концерт, встреча, лекция, распродажа, экскурсия, кино, отдых, развлечение, плакат, афиша, мероприятия' },
+//     { name: 'og:title', content: 'PlPo:place of posters' },
+//     { name: 'og:description', content: 'PlPo:place of posters - место для ваших афиш. Спектакль, концерт, встреча, лекция, распродажа, экскурсия, кино - любое событие может стать плакатом на PlPo' },
+//     { name: 'og:image', content: 'https://plpo.ru/android-chrome-512x512.png' },
+//     { name: 'og:url', content: 'https://plpo.ru/' },
+//   ],
+// })
+// noscript: [
+//     { innerHTML: '<div><img src="https://mc.yandex.ru/watch/97443215" style="position:absolute; left:-9999px;" alt="" /></div>' }
+//   ],
+//   __dangerouslyDisableSanitizersByTagID: {
+//     'noscript': ['innerHTML']
+//   }
+useSeoMeta({
+  charset: 'utf-8',
+  viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0',
   title: 'Место для афиш',
-  meta: [
-    { name: 'description', content: 'PlPo:place of posters - место для ваших афиш. Спектакль, концерт, встреча, лекция, распродажа, экскурсия, кино - любое событие может стать плакатом на PlPo"' },
-    { name: 'keywords', content: 'Спектакль, концерт, встреча, лекция, распродажа, экскурсия, кино, отдых, развлечение, плакат, афиша, мероприятия' },
-    { name: 'og:title', content: 'PlPo:place of posters' },
-    { name: 'og:description', content: 'PlPo:place of posters - место для ваших афиш. Спектакль, концерт, встреча, лекция, распродажа, экскурсия, кино - любое событие может стать плакатом на PlPo' },
-    { name: 'og:image', content: 'https://plpo.ru/android-chrome-512x512.png' },
-    { name: 'og:url', content: 'https://plpo.ru/' },
-  ],
-  // noscript: [
-  //     { innerHTML: '<div><img src="https://mc.yandex.ru/watch/97443215" style="position:absolute; left:-9999px;" alt="" /></div>' }
-  //   ],
-  //   __dangerouslyDisableSanitizersByTagID: {
-  //     'noscript': ['innerHTML']
-  //   }
+  description: 'PlPo:place of posters - место для ваших афиш. Спектакль, концерт, встреча, лекция, распродажа, экскурсия, кино - любое событие может стать плакатом на PlPo"',
+  keywords: 'спектакль, концерт, встреча, лекция, распродажа, экскурсия, кино, отдых, развлечение, плакат, афиша, мероприятия, представление,выступление,собрание,акция,кинопоказ,отпуск,развлечения,баннер,постер,программа,события',
+  ogTitle: 'PlPo:place of posters',
+  ogDescription: 'PlPo:place of posters - место для ваших афиш. Спектакль, концерт, встреча, лекция, распродажа, экскурсия, кино - любое событие может стать плакатом на PlPo',
+  ogImage: 'https://plpo.ru/android-chrome-512x512.png',
+  ogUrl: 'https://plpo.ru/',
 })
-
 </script>
 <template>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,8 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: "page", mode: "out-in" },
     head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1',
       link: [
         { rel: "icon", type: "image/png", href: "/favicon.ico" }
       ]}
